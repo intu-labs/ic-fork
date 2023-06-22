b34c50fd291038d1fcbb52bad4aef00440191a79
Shawn Rader <shawn.t.rader@outlook.com>
2023-06-22 09:40:13 -0700

    Remove unnecessary cast operations

--- rs/crypto/internal/crypto_lib/threshold_sig/bls12_381/src/ni_dkg/fs_ni_dkg/nizk_chunking.rs
+++ rs/crypto/internal/crypto_lib/threshold_sig/bls12_381/src/ni_dkg/fs_ni_dkg/nizk_chunking.rs
@@ -204,14 +204,14 @@ pub fn prove_chunking<R: RngCore + CryptoRng>(
         .check_instance()
         .expect("The chunking proof instance is invalid");
 
-    let m = instance.randomizers_r.len() as u64;
-    let n = instance.public_keys.len() as u64;
+    let m = instance.randomizers_r.len();
+    let n = instance.public_keys.len();
 
-    let ss = n * m * (CHUNK_SIZE as u64 - 1) * CHALLENGE_MASK as u64;
+    let ss = n * m * (CHUNK_SIZE - 1) * CHALLENGE_MASK;
     let zz: u64 = 2 * (NUM_ZK_REPETITIONS as u64) * (ss as u64);
     let range: u64 = zz - 1 + (ss as u64) + 1;
     let zz_big = Scalar::from_u64(zz);
-    let p_sub_s = Scalar::from_u64(ss).neg();
+    let p_sub_s = Scalar::from_usize(ss).neg();
 
     // y0 <- getRandomG1
     let y0 = G1Affine::hash(b"ic-crypto-nizk-chunking-proof-y0", &rng.gen::<[u8; 32]>());
@@ -232,7 +232,7 @@ pub fn prove_chunking<R: RngCore + CryptoRng>(
 
         let first_move = FirstMoveChunking::from(y0.clone(), bb.clone(), cc);
         // Verifier's challenge.
-        let first_challenge = ChunksOracle::new(instance, &first_move).get_all_chunks(n as usize, m as usize);
+        let first_challenge = ChunksOracle::new(instance, &first_move).get_all_chunks(n, m);
 
         // z_s = [sum [e_ijk * s_ij | i <- [1..n], j <- [1..m]] + sigma_k | k <- [1..l]]
 
@@ -340,10 +340,10 @@ pub fn verify_chunking(
     require_eq("z_r", nizk.z_r.len(), num_receivers)?;
     require_eq("z_s", nizk.z_s.len(), NUM_ZK_REPETITIONS)?;
 
-    let m = instance.randomizers_r.len() as u64;
-    let n = instance.public_keys.len() as u64;
-    let ss = n * m * (CHUNK_SIZE as u64 - 1) * CHALLENGE_MASK as u64;
-    let zz = 2 * (NUM_ZK_REPETITIONS as u64) * ss;
+    let m = instance.randomizers_r.len();
+    let n = instance.public_keys.len();
+    let ss = n * m * (CHUNK_SIZE - 1) * CHALLENGE_MASK;
+    let zz = 2 * (NUM_ZK_REPETITIONS as u64) * (ss as u64);
     let zz_big = Scalar::from_u64(zz);
 
     for z_sk in nizk.z_s.iter() {
@@ -355,7 +355,7 @@ pub fn verify_chunking(
     let first_move = FirstMoveChunking::from(nizk.y0.clone(), nizk.bb.clone(), nizk.cc.clone());
     let second_move = SecondMoveChunking::from(&nizk.z_s, &nizk.dd, &nizk.yy);
     // e_{m,n,l} = oracle(instance, y_0, bb, cc)
-    let e = ChunksOracle::new(instance, &first_move).get_all_chunks(n as usize, m as usize);
+    let e = ChunksOracle::new(instance, &first_move).get_all_chunks(n, m);
 
     // x = oracle(e, z_s, dd, yy)
     let x = chunking_proof_challenge_oracle(&e, &second_move);
@@ -416,7 +416,7 @@ pub fn verify_chunking(
                 .flatten()
                 .map(|e_ij| Scalar::from_u64(e_ij[k]))
                 .collect();
-            if c_ij_s.len() as u64 != m * n || e_ijk_s.len() as u64 != m * n {
+            if c_ij_s.len() != m * n || e_ijk_s.len() != m * n {
                 return Err(ZkProofChunkingError::InvalidProof);
             }
 
